d3b2620b5b583c07ac9abfcb22ea2b93e14d510a
Wilver Fabian Rojas Del Ducca <wilverfabian.06@gmail.com>
2024-01-15 17:55:19 -0500

    Feat: Remove unused keys

--- lib/app/models/liveness.dart
+++ lib/app/models/liveness.dart
@@ -15,62 +15,22 @@ class Liveness {
 }
 
 class Data {
-  String status;
-  String searchMode;
-  List<String> images;
-  String id;
-  bool deleted;
-  String client;
-  String type;
-  String os;
   double livenessMinScore;
   Result result;
-  DateTime updatedAt;
-  DateTime createdAt;
 
   Data({
-    required this.status,
-    required this.searchMode,
-    required this.images,
-    required this.id,
-    required this.deleted,
-    required this.client,
-    required this.type,
-    required this.os,
     this.livenessMinScore = 0.0,
     required this.result,
-    required this.updatedAt,
-    required this.createdAt,
   });
 
   factory Data.fromJson(Map<String, dynamic> json) => Data(
-        status: json["status"] ?? '',
-        searchMode: json["search_mode"] ?? '',
-        images: List<String>.from(json["images"].map((x) => x)),
-        id: json["_id"] ?? '',
-        deleted: json["deleted"] ?? false,
-        client: json["client"] ?? '',
-        type: json["type"] ?? '',
-        os: json["os"] ?? '',
         livenessMinScore: json["liveness_min_score"]?.toDouble(),
         result: Result.fromJson(json["result"]),
-        updatedAt: DateTime.parse(json["updatedAt"]),
-        createdAt: DateTime.parse(json["createdAt"]),
       );
 
   Map<String, dynamic> toJson() => {
-        "status": status,
-        "search_mode": searchMode,
-        "images": List<dynamic>.from(images.map((x) => x)),
-        "_id": id,
-        "deleted": deleted,
-        "client": client,
-        "type": type,
-        "os": os,
         "liveness_min_score": livenessMinScore,
         "result": result.toJson(),
-        "updatedAt": updatedAt.toIso8601String(),
-        "createdAt": createdAt.toIso8601String(),
       };
 }
 
